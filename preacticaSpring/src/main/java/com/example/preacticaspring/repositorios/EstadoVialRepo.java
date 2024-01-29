d6c3cfbd903d92a20f80b6fd3b3d44bce09203e6
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-28 22:49:41 -0500

    Update EstadoVialRepo.java

--- preacticaSpring/src/main/java/com/example/preacticaspring/repositorios/EstadoVialRepo.java
+++ preacticaSpring/src/main/java/com/example/preacticaspring/repositorios/EstadoVialRepo.java
@@ -1,2 +1,12 @@
-package com.example.preacticaspring.repositorios;public interface EstadoVialRepo {
+package com.example.preacticaspring.repositorios;
+
+
+import com.example.preacticaspring.modelos.EstadoVial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface EstadoVialRepo extends JpaRepository<EstadoVial, Long> {
+
+
+
 }
