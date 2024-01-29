cbfa3b07907286fff1529d8796b254e2f4dead2a
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-28 22:49:36 -0500

    Update

--- preacticaSpring/src/main/java/com/example/preacticaspring/modelos/EstadoVial.java
+++ preacticaSpring/src/main/java/com/example/preacticaspring/modelos/EstadoVial.java
@@ -1,4 +1,21 @@
 package com.example.preacticaspring.modelos;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tbl_estados_viales")
+@Data
+@ToString
 public class EstadoVial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_estadoVial;
+    private Boolean estado;
+    private String temporada;
+    private LocalDate fecha;
+
 }
