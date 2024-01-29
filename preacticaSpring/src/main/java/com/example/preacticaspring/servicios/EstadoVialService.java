8d6b6e187a0f67c4ee49c8e204ae40df6e653a80
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-28 22:56:30 -0500

    Create EstadoVialService.java

---
+++ preacticaSpring/src/main/java/com/example/preacticaspring/servicios/EstadoVialService.java
@@ -0,0 +1,40 @@
+package com.example.preacticaspring.servicios;
+
+import com.example.preacticaspring.modelos.EstadoVial;
+import com.example.preacticaspring.repositorios.EstadoVialRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EstadoVialService {
+    @Autowired
+    private EstadoVialRepo repo;
+
+    //metodo CRUD
+    public EstadoVial guardar(EstadoVial estadoVial){
+        return repo.save(estadoVial);
+    }
+
+    //listar
+    public List<EstadoVial> listar(){
+        return repo.findAll();
+    }
+
+    //encontrarPorID
+    //OPTIONAL, CLASE
+    Optional<EstadoVial> encontrarPorId(Long id_estadoVial){
+        return repo
+                .findById(id_estadoVial);
+    }
+
+    //eliminar
+    public void eliminar(Long id_estadoVial){
+        repo.deleteById(id_estadoVial);
+    }
+
+
+
+}
