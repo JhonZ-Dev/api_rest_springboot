3daba48eae6aa69ea77c1a2277903c1575e8ce27
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-30 23:13:59 -0500

    Update EstadoVialController.java

--- preacticaSpring/src/main/java/com/example/preacticaspring/controller/EstadoVialController.java
+++ preacticaSpring/src/main/java/com/example/preacticaspring/controller/EstadoVialController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v1")
+
 public class EstadoVialController {
     @Autowired
     public EstadoVialService service;
